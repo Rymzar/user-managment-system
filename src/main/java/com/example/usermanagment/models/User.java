f0a205dad5a8a612e3f0fa581ef0855313602398
Nikta Zemlianskikh <rymzar@gmail.com>
2024-01-31 23:08:15 +0300

    register controller

--- src/main/java/com/example/usermanagment/models/User.java
+++ src/main/java/com/example/usermanagment/models/User.java
@@ -8,7 +8,7 @@ import lombok.Data;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
     private String login;
     private String email;
     private String phone;
